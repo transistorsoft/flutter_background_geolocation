9ebff3a1999557792c387ffaadda182127e02442
Chris Scott <chris@transistorsoft.com>
2022-12-12 11:13:56 -0500

    [RELEASE] 4.8.5

--- example/lib/advanced/home_view.dart
+++ example/lib/advanced/home_view.dart
@@ -152,7 +152,6 @@ class HomeViewState extends State<HomeView> with TickerProviderStateMixin<HomeVi
     )).then((bg.State state) async {
       print('[ready] ${state.toMap()}');
       print('[didDeviceReboot] ${state.didDeviceReboot}');
-
       if (state.schedule.isNotEmpty) {
         bg.BackgroundGeolocation.startSchedule();
       }
@@ -275,7 +274,7 @@ class HomeViewState extends State<HomeView> with TickerProviderStateMixin<HomeVi
     bg.BackgroundGeolocation.playSound(util.Dialog.getSoundId("BUTTON_CLICK"));
 
     bg.BackgroundGeolocation.getCurrentPosition(
-        persist: false,       // <-- do not persist this location
+        persist: true,       // <-- do not persist this location
         desiredAccuracy: 40, // <-- desire an accuracy of 40 meters or less
         maximumAge: 10000,   // <-- Up to 10s old is fine.
         timeout: 30,         // <-- wait 30s before giving up.
