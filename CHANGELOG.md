40f9f3509f24f8fcfaf8115e2c5cd2b2f787ccff
Chris Scott <christocracy@gmail.com>
2024-03-19 23:15:23 -0400

    [iOS] Implement new iOS PrivacyManifest.  Add new AuthorizationEvent.status property to return the HTTP status from your Authorization.refreshUrl

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # CHANGELOG
 
-## 4.14.3 &mdash; 2024-03-19
+## 4.15.0 &mdash; 2024-03-19
+* [iOS] Implement new [iOS Privacy Manifest](https://developer.apple.com/documentation/bundleresources/privacy_manifest_files?language=objc)
+* Add property `AuthorizationEvent.status`, provding the HTTP status code from the `refreshUrl`.
+
+## 4.14.3 &mdash; 2024-03-18
 * [iOS] Fix bug in iOS scheduler, triggering ON incorrectly.  For example, given a `schedule: ['1 00:00-23:59'], the plugin was trigging on for `DAY 2`.
 
 ## 4.14.2 &mdash; 2024-03-14
--- lib/models/authorization_event.dart
+++ lib/models/authorization_event.dart
@@ -3,6 +3,9 @@ part of '../flutter_background_geolocation.dart';
 /// Event object provided to [BackgroundGeolocation.onAuthorization].
 ///
 class AuthorizationEvent {
+  /// HTTP Status returned from your [Authorization.refreshURl] (or `0` if the HTTP request failed).
+  late int status;
+
   /// `true` when an authorization request to [Authorization.refreshUrl] was successful.
   ///
   late bool success;
@@ -14,12 +17,13 @@ class AuthorizationEvent {
   Map? response;
 
   AuthorizationEvent(Map data) {
+    status = _ensureInt(data['status'])!;
     success = _ensureBool(data['success'])!;
     error = data['error'];
     response = data['response'];
   }
 
   String toString() {
-    return "[AuthorizationEvent success=$success, error=$error, response=$response]";
+    return "[AuthorizationEvent success=$success, status=$status error=$error, response=$response]";
   }
 }
